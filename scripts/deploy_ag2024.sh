244cfb248a8eebb2a70b502e3aec6f2bbc0f370d
drosofff <drosofff@gmail.com>
2024-11-10 16:28:24 +0100

    Update deploy_ag2024.sh

--- scripts/deploy_ag2024.sh
+++ scripts/deploy_ag2024.sh
@@ -5,8 +5,6 @@ apt install python3-pip python3-dev
 pip install -U pip
 source .bashrc
 pip install ansible==3.0.0
-git clone https://github.com/artbio/galaxyXpand -b ag2024
-cd galaxyXpand
 ansible-galaxy install -r requirements.yml -p roles/
 time ansible-playbook -i environments/ag2024/hosts playbook.yml
 time ansible-playbook -i environments/ag2024/hosts install_tools.yml
