d95ebe5bc8a88f8c1079d2f4028554aa57e911d7
drosofff <drosofff@gmail.com>
2024-11-10 16:34:36 +0100

    Update deploy_ag2024.sh

--- scripts/deploy_ag2024.sh
+++ scripts/deploy_ag2024.sh
@@ -3,7 +3,7 @@ date
 apt update
 apt install python3-pip python3-dev -y
 pip install -U pip
-source .bashrc
+source ~/.bashrc
 pip install ansible==3.0.0
 ansible-galaxy install -r requirements.yml -p roles/
 time ansible-playbook -i environments/ag2024/hosts playbook.yml
