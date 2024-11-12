f81fb58cb581cd7d26e837415d5f374eea56ac7f
drosofff <drosofff@gmail.com>
2024-11-13 00:43:30 +0100

    Update deploy_ag2024.sh

--- scripts/deploy_ag2024.sh
+++ scripts/deploy_ag2024.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-date | tee install_log.txt
-apt update
-apt upgrade -y
-apt install python3-pip python3-dev -y
-pip install -U pip
-/usr/local/bin/pip install ansible==3.0.0
-ansible-galaxy install -r requirements.yml -p roles/ -f | tee install_log.txt
-time ansible-playbook -i environments/ag2024/hosts playbook.yml | tee install_log.txt
-time ansible-playbook -i environments/ag2024/hosts install_tools.yml | tee install_log.txt
-date
+date | tee ~/install_log.txt
+apt update | tee ~/install_log.txt
+apt upgrade -y | tee ~/install_log.txt
+apt install python3-pip python3-dev -y | tee ~/install_log.txt
+pip install -U pip | tee ~/install_log.txt
+/usr/local/bin/pip install ansible==3.0.0 | tee ~/install_log.txt
+ansible-galaxy install -r requirements.yml -p roles/ -f | tee ~/install_log.txt
+time ansible-playbook -i environments/ag2024/hosts playbook.yml | tee ~/install_log.txt
+sleep 60
+systemctl restart galaxy.target
+sleep 30
+galaxyctl status | tee ~/install_log.txt
+time ansible-playbook -i environments/ag2024/hosts install_tools.yml | tee ~/install_log.txt
+date | tee ~/install_log.txt
