c7198e4b5b5b022dfe485b1eb3c9e7e00f3fce9e
drosofff <drosofff@gmail.com>
2024-11-13 12:21:52 +0100

    Update deploy_ag2024.sh

--- scripts/deploy_ag2024.sh
+++ scripts/deploy_ag2024.sh
@@ -6,10 +6,10 @@ apt install python3-pip python3-dev -y >> ~/install_log.txt
 pip install -U pip >> ~/install_log.txt
 /usr/local/bin/pip install ansible==3.0.0 >> ~/install_log.txt
 ansible-galaxy install -r ~/galaxyXpand/requirements.yml -p ~/galaxyXpand/roles/ -f >> ~/install_log.txt
-time ansible-playbook -i ~/galaxyXpand/environments/ag2024/hosts playbook.yml >> ~/install_log.txt
+time ansible-playbook -i ~/galaxyXpand/environments/ag2024/hosts ~/galaxyXpand/playbook.yml >> ~/install_log.txt
 sleep 60
 systemctl restart galaxy.target
 sleep 30
 galaxyctl status >> ~/install_log.txt
-time ansible-playbook -i ~/galaxyXpand/environments/ag2024/hosts install_tools.yml >> ~/install_log.txt
+time ansible-playbook -i ~/galaxyXpand/environments/ag2024/hosts ~/galaxyXpand/install_tools.yml >> ~/install_log.txt
 date >> ~/install_log.txt
