5fdeae545066d79363627d534a72d1c1b29d5a22
drosofff <drosofff@gmail.com>
2024-11-18 12:05:51 +0100

    Update deploy_ag2024.sh

--- scripts/deploy_ag2024.sh
+++ scripts/deploy_ag2024.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 date >> ~/install_log.txt
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFdDo/U2shXOKSwSz1JhYY7jDCwuAt4sgPH2pw0r1fKmxJpsY7vZgwkn1XvZPDPPo9Go2LgNVCRBgRZ60f1ivlviy5D1deo/5XtMshDjJDSBaGx4QQhtjAsCgvZ/2Sx+wP2l7IVEYKzWdKens4JVZ+gIh/PwXS0PzNeWOLiCynlLncEm0nuV9Y6wCbhnUt9Zjbe/y0/Cm6wPpOqu9J2BKVerGZtnfvM2+MD6S40n4CH7iFRrnqlOlc6junnuJ6g/u2sIcYXhmD6bOttI5cOINGcDacitp7enUaiSt5ViVz0vjkHfuxr42rtq5dLMI6hQxnq8sjpfb1ygpe7HHHnHCn chris@lbcd-17.snv.jussieu.fr" >> /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+# exemple of access: ssh -i ~/.ssh/id_rsa_chrisartbio root@34.105.216.34
 apt update >> ~/install_log.txt
 apt upgrade -y >> ~/install_log.txt
 apt install python3-pip python3-dev -y >> ~/install_log.txt
